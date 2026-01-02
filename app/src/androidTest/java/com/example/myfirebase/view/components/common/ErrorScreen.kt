91f42ed9a7e42492f18414ef5adecb1dd2bf256b
galang48 <galangyudha1945@gmail.com>
2026-01-02 20:59:22 +0700

    Tambahkan composable untuk layar galat
    Menambahkan composable `ErrorScreen` yang menampilkan pesan galat dan tombol coba lagi.

---
+++ app/src/androidTest/java/com/example/myfirebase/view/components/common/ErrorScreen.kt
@@ -0,0 +1,33 @@
+package com.example.myfirebase.view.components.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.myfirebase.R
+
+@Composable
+fun ErrorScreen(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.gagal),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
