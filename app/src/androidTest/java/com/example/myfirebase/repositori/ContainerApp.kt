261e60aeed8249e004641e202f331f47d17a7187
galang48 <galangyudha1945@gmail.com>
2026-01-02 20:11:28 +0700

    Menerapkan container aplikasi default
    Menambahkan kelas `DefaultContainerApp` yang mengimplementasikan antarmuka `ContainerApp` untuk menyediakan instance `FirebaseRepositorySiswa` secara lazy.

--- app/src/androidTest/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/androidTest/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -4,3 +4,9 @@ package com.example.myfirebase.repositori
 interface ContainerApp {
     val repositorySiswa: RepositorySiswa
 }
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
