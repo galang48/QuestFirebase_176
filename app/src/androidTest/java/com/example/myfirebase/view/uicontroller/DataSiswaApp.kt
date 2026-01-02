a6269ae53a2c80d9a92cbab2b6506709c2a85733
galang48 <galangyudha1945@gmail.com>
2026-01-02 21:16:04 +0700

    Menambahkan HostNavigasi
    Membuat composable `HostNavigasi` untuk mengatur navigasi aplikasi menggunakan Jetpack Compose Navigation.
    
    Fungsi ini mendefinisikan rute untuk:
    - `HomeScreen` sebagai tujuan awal.
    - `EntrySiswaScreen` untuk menambah data siswa.
    - Navigasi dari `HomeScreen` ke `EntrySiswaScreen`.
    - Navigasi kembali dari `EntrySiswaScreen`.

---
+++ app/src/androidTest/java/com/example/myfirebase/view/uicontroller/DataSiswaApp.kt
@@ -0,0 +1,17 @@
+package com.example.myfirebase.view.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(
+        navController = navController,
+        modifier = modifier
+    )
+}
