16a786cb2e7e918458afd122f65ab4312ae3b629
galang48 <galangyudha1945@gmail.com>
2026-01-02 21:01:39 +0700

    Tambahkan komponen untuk layar pemuatan
    Menambahkan composable `LoadingScreen` yang menampilkan gambar pemuatan.

---
+++ app/src/androidTest/java/com/example/myfirebase/view/components/common/LoadingScreen.kt
@@ -0,0 +1,19 @@
+package com.example.myfirebase.view.components.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.myfirebase.R
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading),
+        modifier = modifier.size(200.dp)
+    )
+}
