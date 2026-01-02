b5a5913b5798c6552589bdb3f8be25fd0ef6072d
galang48 <galangyudha1945@gmail.com>
2026-01-02 20:34:32 +0700

    Refactor: Pindahkan ContainerApp ke package container
    Memindahkan file `ContainerApp.kt` dari package `com.example.myfirebase.repositori` ke `com.example.myfirebase.repository.container`.
    
    Perubahan ini juga mencakup:
    - Menghapus implementasi `DefaultContainerApp` dan kelas `AplikasiDataSiswa` dari file tersebut.
    - Memperbarui deklarasi package menjadi `com.example.myfirebase.repository.container`.

---
+++ app/src/androidTest/java/com/example/myfirebase/repository/container/ContainerApp.kt
@@ -0,0 +1,7 @@
+package com.example.myfirebase.repository.container
+
+import com.example.myfirebase.repository.RepositorySiswa
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
