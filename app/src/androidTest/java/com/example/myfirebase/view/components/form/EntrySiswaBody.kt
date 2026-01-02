40a0287c6990b83e5d72f731aff447d746c54b78
galang48 <galangyudha1945@gmail.com>
2026-01-02 21:09:24 +0700

    Tambahkan composable `EntrySiswaBody`
    Menambahkan composable `EntrySiswaBody` yang berisi:
    - `FormTambahSiswa` untuk input data.
    - Tombol `Text` untuk submit, yang mengambil string dari `R.string.btn_submit`.

---
+++ app/src/androidTest/java/com/example/myfirebase/view/components/form/EntrySiswaBody.kt
@@ -0,0 +1,43 @@
+package com.example.myfirebase.view.components.form
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import com.example.myfirebase.R
+import com.example.myfirebase.model.data.DetailSiswa
+import com.example.myfirebase.model.data.UIStateSiswa
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(R.dimen.padding_medium))
+    ) {
+        FormTambahSiswa(
+            detailSiswa = uiStateSiswa.detailSiswa,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateSiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
