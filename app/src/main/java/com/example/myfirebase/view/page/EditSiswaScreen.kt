20f83ff266b66c1005a5224e7ba0d0329575f497
galang48 <galangyudha1945@gmail.com>
2026-01-09 14:02:46 +0700

    Hapus baris kosong berlebih di EditSiswaScreen.kt
    Menghapus baris kosong yang tidak diperlukan di antara deklarasi package dan import.

--- app/src/main/java/com/example/myfirebase/view/page/EditSiswaScreen.kt
+++ app/src/main/java/com/example/myfirebase/view/page/EditSiswaScreen.kt
@@ -1,6 +1,5 @@
 package com.example.myfirebase.view.page
 
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
