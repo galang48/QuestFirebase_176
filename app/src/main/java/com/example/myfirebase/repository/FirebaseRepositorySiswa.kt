8389b8a73d8173ec46e84e469acda1ae935fa31b
galang48 <galangyudha1945@gmail.com>
2026-01-09 13:00:30 +0700

    Menambahkan fungsi editSatuSiswa pada FirebaseRepositorySiswa
    Mengimplementasikan fungsi `editSatuSiswa` untuk memperbarui data siswa berdasarkan ID di Firestore.

--- app/src/main/java/com/example/myfirebase/repository/FirebaseRepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repository/FirebaseRepositorySiswa.kt
@@ -53,5 +53,22 @@ class FirebaseRepositorySiswa : RepositorySiswa {
                 println("Gagal baca data siswa : ${e.message}")
                 null
             }
+        override suspend fun editSatuSiswa(
+            id: Long,
+            siswa: Siswa,
+        ) {
+            val docQuery = collection.whereEqualTo("id", id).get().await()
+            val docId = docQuery.documents.firstOrNull()?.id ?: return
+            collection
+                .document(docId)
+                .set(
+                    mapOf(
+                        "id" to siswa.id,
+                        "nama" to siswa.nama,
+                        "alamat" to siswa.alamat,
+                        "telpon" to siswa.telpon,
+                    ),
+                ).await()
+        }
     }
 }
