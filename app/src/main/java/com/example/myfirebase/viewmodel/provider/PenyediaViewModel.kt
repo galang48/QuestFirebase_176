4c6790c282b999999e8abf17775bd7dd97325287
galang48 <galangyudha1945@gmail.com>
2026-01-09 13:30:07 +0700

    Menambahkan DetailViewModel dan EditViewModel ke PenyediaViewModel
    Menambahkan initializer untuk `DetailViewModel` dan `EditViewModel` ke dalam `PenyediaViewModel` menggunakan `createSavedStateHandle` dan `repositorySiswa`.

--- app/src/main/java/com/example/myfirebase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,21 +1,38 @@
+
 package com.example.myfirebase.viewmodel.provider
 
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myfirebase.viewmodel.DetailViewModel
+import com.example.myfirebase.viewmodel.EditViewModel
 import com.example.myfirebase.viewmodel.EntryViewModel
 import com.example.myfirebase.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
-    val Factory = viewModelFactory {
-        initializer {
-            HomeViewModel(
-                aplikasiDataSiswa().container.repositorySiswa
-            )
+    val Factory =
+        viewModelFactory {
+            initializer {
+                HomeViewModel(
+                    aplikasiDataSiswa().container.repositorySiswa,
+                )
+            }
+            initializer {
+                EntryViewModel(
+                    aplikasiDataSiswa().container.repositorySiswa,
+                )
+            }
+            initializer {
+                DetailViewModel(
+                    this.createSavedStateHandle(),
+                    aplikasiDataSiswa().container.repositorySiswa,
+                )
+            }
+            initializer {
+                EditViewModel(
+                    this.createSavedStateHandle(),
+                    aplikasiDataSiswa().container.repositorySiswa,
+                )
+            }
         }
-        initializer {
-            EntryViewModel(
-                aplikasiDataSiswa().container.repositorySiswa
-            )
-        }
-    }
 }
