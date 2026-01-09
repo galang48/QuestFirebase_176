362e9359980f4794d34bf13fe293f8ab9b99770b
galang48 <galangyudha1945@gmail.com>
2026-01-09 13:14:57 +0700

    Menambahkan logika ViewModel untuk Detail Siswa
    Mengimplementasikan `DetailViewModel` dengan fitur sebagai berikut:
    - Mengambil `idSiswa` dari `SavedStateHandle`.
    - Mengelola state UI menggunakan sealed interface `StatusUIDetail`.
    - Menambahkan fungsi `getSatuSiswa` untuk mengambil data siswa berdasarkan ID dari repository.

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myfirebase.model.data.Siswa
+import com.example.myfirebase.repository.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUIDetail {
     data class Success(
@@ -10,4 +20,34 @@ sealed interface StatusUIDetail {
     object Error : StatusUIDetail
 
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa,
+) : ViewModel() {
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail =
+                try {
+                    StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa))
+                } catch (e: IOException) {
+                    StatusUIDetail.Error
+                } catch (e: Exception) {
+                    StatusUIDetail.Error
+                }
+        }
+    }
 }
