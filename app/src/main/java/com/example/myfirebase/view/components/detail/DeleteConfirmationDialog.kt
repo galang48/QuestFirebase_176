d209d1ce2a27d8d00bdc5b28b98f8a29cae4458e
galang48 <galangyudha1945@gmail.com>
2026-01-09 13:54:50 +0700

    Menambahkan komponen DeleteConfirmationDialog
    Menambahkan dialog konfirmasi penghapusan menggunakan Material3 AlertDialog yang menyertakan tombol konfirmasi (yes) dan pembatalan (no).

---
+++ app/src/main/java/com/example/myfirebase/view/components/detail/DeleteConfirmationDialog.kt
@@ -0,0 +1,33 @@
+package com.example.myfirebase.view.components.detail
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.myfirebase.R
+
+@Composable
+fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.tanya)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.yes))
+            }
+        },
+    )
+}
