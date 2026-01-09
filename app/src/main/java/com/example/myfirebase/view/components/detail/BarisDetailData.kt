b4c212d340d5e9f89012d1b8e2f5c2138981b754
galang48 <galangyudha1945@gmail.com>
2026-01-09 13:39:33 +0700

    Menambahkan komponen BarisDetailData
    Menambahkan composable `BarisDetailData` untuk menampilkan detail data dalam format baris dengan label dan nilai.

---
+++ app/src/main/java/com/example/myfirebase/view/components/detail/BarisDetailData.kt
@@ -0,0 +1,23 @@
+package com.example.myfirebase.view.components.detail
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun BarisDetailData(
+    @StringRes labelResID: Int,
+    itemDetail: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
