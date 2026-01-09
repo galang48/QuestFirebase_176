796a78e691364bf85f85e386754b33299654ae71
galang48 <galangyudha1945@gmail.com>
2026-01-09 13:14:10 +0700

    Tambah fungsi getSatuSiswa ke RepositorySiswa
    Menambahkan fungsi `getSatuSiswa(id: Long)` pada interface `RepositorySiswa` untuk mengambil data siswa berdasarkan ID.

--- app/src/main/java/com/example/myfirebase/repository/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repository/RepositorySiswa.kt
@@ -5,6 +5,7 @@ import com.example.myfirebase.model.data.Siswa
 interface RepositorySiswa {
     suspend fun getDataSiswa(): List<Siswa>
     suspend fun postDataSiswa(siswa: Siswa)
+    suspend fun getSatuSiswa(id: Long): Siswa?
     suspend fun editSatuSiswa(
         id: Long,
         siswa: Siswa,
